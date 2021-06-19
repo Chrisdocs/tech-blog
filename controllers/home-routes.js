de41fb23f1ddcabe39f7500e158470cc55abc91d
Chris Wolfe <chrisa.wolfe@gmail.com>
2021-06-19 17:50:12 -0500

    created hompage, login and session functions and pages

---
+++ controllers/home-routes.js
@@ -0,0 +1,44 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
+
+router.get('/', (req, res) => {
+    console.log(req.session);
+
+    Post.findAll({
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(postDbData => {
+        const posts = postDbData.map(post => post.get({ plain: true }));
+        console.log(postDbData[0])
+        res.render('homepage', { posts });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+    
+    res.render('login');
+})
+
+module.exports = router;
