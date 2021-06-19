a5b54a6c92d84242a0692278dd6549fda4dd72f6
Chris Wolfe <chrisa.wolfe@gmail.com>
2021-06-19 18:23:30 -0500

    single post comments and user experiance for logging in and out

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -13,19 +13,22 @@ router.get('/', (req, res) => {
 
 // Get one comment /api/comments/:id
 
-// Create a comment /api/comments
 router.post('/', (req, res) => {
-    Comment.create({
-        comment_text: req.body.comment_text,
-        user_id: req.body.user_id,
-        post_id: req.body.post_id
-    })
-    .then(commentDbData => res.json(commentDbData))
-    .catch(err => {
-        console.log(err);
-        res.status(400).json(err);
-    })
-})
+    // check the session
+    if (req.session) {
+        Comment.create({
+                comment_text: req.body.comment_text,
+                post_id: req.body.post_id,
+                // use the id from the session
+                user_id: req.session.user_id
+            })
+            .then(dbCommentData => res.json(dbCommentData))
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    }
+});
 
 // Update a comment /api/comments/:id
 
